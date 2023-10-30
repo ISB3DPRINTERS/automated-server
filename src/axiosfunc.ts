9afd7d1b900f25b5c4c6e6a3029f9e2705da2387
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-10-29 17:40:18 -0700

    Fixed the axios function request

--- src/axiosfunc.ts
+++ src/axiosfunc.ts
@@ -1,19 +1,23 @@
 import { findapikeys, apipathfinder, passwordarray } from "./functions";
 import axios, { isCancel, AxiosError } from "axios";
 
-export default async function axiosfunc (grade:number) {
+export default async function axiosfunc (grade:number,printer:number) {
     console.log("axiosfunc")
-    axios.post(apipathfinder(grade)[, data[, config]])
-    /*axios.put({
-    method: "put",
-    url: apipathfinder(grade),
-    body: {
-      password: passwordarray(grade,printer,"new"),
-      current: passwordarray(grade,printer,"old"),
-    },
-    headers: {
-      "X-Api-Key": findapikeys(grade, printer),
-    },
-  });
-*/
-}
+    axios.post(apipathfinder(grade), {
+      body:{
+        password: passwordarray(grade,printer,"new"),
+        current: passwordarray(grade,printer,"old"),
+      },
+      headers: {
+        "X-Api-Key": findapikeys(grade, printer),
+      },
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    console.log("axios func finished redid printer"+printer+"grade"+grade)
+  }
